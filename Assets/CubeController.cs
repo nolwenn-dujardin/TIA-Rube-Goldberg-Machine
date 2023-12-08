4ba55d3e620b7c727d1ae1021430c199972a3fa6
Dujardin Nolwenn <nolwenn.dujardin@etudiant.univ-rennes1.fr>
2023-12-08 12:38:22 +0100

    Add UI for GameObject translate and rotation

--- Assets/CubeController.cs
+++ Assets/CubeController.cs
@@ -10,23 +10,52 @@ public class CubeController : MonoBehaviour
         public Slider sliderY;
         public Slider sliderZ;
 
+        public Vector3 initialPos;
+
+        public int minPos = -1;
+        public int maxPos = 1;
+
+        public int minRotate = 0;
+        public int maxRotate = 360;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        initialPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         if(rotate){
-            transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
-        }
+            //transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+            
+            var rot = transform.localRotation.eulerAngles;
+            rot.Set(sliderX.value,
+            sliderY.value, 
+            sliderZ.value);
 
-        transform.position = new Vector3(sliderX.value,sliderY.value, sliderZ.value);
+            transform.localRotation = Quaternion.Euler(rot);
+        }
+        else {
+           transform.position = initialPos + new Vector3(sliderX.value,
+            sliderY.value, 
+            sliderZ.value); 
+        }
     }
 
     public void Rotation(){
         rotate = !rotate;
+        sliderX.value = sliderY.value = sliderZ.value = 0;
+
+        if(rotate){
+            sliderX.minValue = sliderY.minValue = sliderZ.minValue = minRotate;
+            sliderX.maxValue = sliderY.maxValue = sliderZ.maxValue = maxRotate;
+
+        }
+        else {
+            sliderX.minValue = sliderY.minValue = sliderZ.minValue = minPos;
+            sliderX.maxValue = sliderY.maxValue = sliderZ.maxValue = maxPos;
+        }
     }
 }
