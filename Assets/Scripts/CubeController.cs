21999d045c77ab3d89b19192970fd919bb9d725c
Dujardin Nolwenn <nolwenn.dujardin@etudiant.univ-rennes1.fr>
2023-12-14 09:15:28 +0100

    Amélioration déplacement des objets
    Ajout sélection d'un objet à bouger par un click dessus

--- Assets/Scripts/CubeController.cs
+++ Assets/Scripts/CubeController.cs
@@ -1,59 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class CubeController : MonoBehaviour
 {
-        private bool rotate = false;
-        public Slider sliderX;
-        public Slider sliderY;
-        public Slider sliderZ;
-
-        public Vector3 initialPos;
-
-        public int minPos = -1;
-        public int maxPos = 1;
-
-        public int minRotate = 0;
-        public int maxRotate = 360;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        initialPos = transform.position;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(rotate){
-            var rot = transform.localRotation.eulerAngles;
-            rot.Set(sliderX.value,
-            sliderY.value, 
-            sliderZ.value);
-
-            transform.localRotation = Quaternion.Euler(rot);
+    private void OnMouseDown() {
+        Debug.Log("Click on cube");
+        if (GameManager.Manager != null)
+        {
+            GameManager.Manager.SelectObject(gameObject);
         }
-        else {
-           transform.position = initialPos + new Vector3(sliderX.value,
-            sliderY.value, 
-            sliderZ.value); 
-        }
-    }
-
-    public void Rotation(){
-        rotate = !rotate;
-        sliderX.value = sliderY.value = sliderZ.value = 0;
-
-        if(rotate){
-            sliderX.minValue = sliderY.minValue = sliderZ.minValue = minRotate;
-            sliderX.maxValue = sliderY.maxValue = sliderZ.maxValue = maxRotate;
-
-        }
-        else {
-            sliderX.minValue = sliderY.minValue = sliderZ.minValue = minPos;
-            sliderX.maxValue = sliderY.maxValue = sliderZ.maxValue = maxPos;
+        else
+        {
+            Debug.LogWarning("GameManager.Manager is null!");
         }
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Manager;
 
     public GameObject selectedObject;
+
+    public TMP_Text posOrRot;
+
+    public bool objectIsSelect = false;
 
     private bool rotate = false;
     public Slider sliderX;
@@ -23,67 +28,107 @@ public class GameManager : MonoBehaviour
     public int minRotate = 0;
     public int maxRotate = 360;
 
+    public void Awake()
+    {
+        if(GameManager.Manager != null) { Destroy(this); }
+        Manager = this;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         sliderX.minValue = sliderY.minValue = sliderZ.minValue = minPos;
         sliderX.maxValue = sliderY.maxValue = sliderZ.maxValue = maxPos;
-        initialPos = selectedObject.transform.localPosition;
+        //initialPos = selectedObject.transform.localPosition;
+        //initialRot = selectedObject.transform.localRotation.eulerAngles;
+
+        posOrRot.text = "Position";
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(rotate){
-            var rot = selectedObject.transform.localRotation.eulerAngles;
-            rot.Set(sliderX.value,
-            sliderY.value, 
-            sliderZ.value);
+        if(objectIsSelect){
+            if(rotate){
+                var rot = selectedObject.transform.localRotation.eulerAngles;
+                rot.Set(sliderX.value,
+                sliderY.value, 
+                sliderZ.value);
 
-            selectedObject.transform.localRotation = Quaternion.Euler(rot);
-        }
-        else {
+                selectedObject.transform.localRotation = Quaternion.Euler(rot);
+            }
+            else {
             /*Work if object pos is (0,0,0) -> in other case, 
             modify min and max value of slider for position to be (initialPos-minValue,initialPos+maxValue)
            */
-           selectedObject.transform.localPosition = new Vector3(sliderX.value,
-            sliderY.value, 
-            sliderZ.value); 
+                selectedObject.transform.localPosition = new Vector3(sliderX.value,
+                sliderY.value, 
+                sliderZ.value); 
+            }
+        }
+        
+    }
+
+    public void Rotation(){
+        if(objectIsSelect){
+            rotate = !rotate;
+            //sliderX.value = sliderY.value = sliderZ.value = 0;
+
+            if(rotate){
+                //Save object position
+                initialPos = selectedObject.transform.localPosition;
+
+                //Restore rotation
+                selectedObject.transform.localRotation = Quaternion.Euler(initialRot);
+
+                initForChangeRot();
+            }
+            else {
+                //Save rotation
+                initialRot = selectedObject.transform.localRotation.eulerAngles;
+
+                //Restore object position
+                selectedObject.transform.localPosition = initialPos;
+
+                initForChangePos();
+            }
+
+            Debug.Log("Rotate is "+rotate);
         }
     }
 
-    //TODO Save rotation value
+    //Init slider value for allowing to change position of gameObject
+    void initForChangePos(){
+        sliderX.minValue = sliderY.minValue = sliderZ.minValue = minPos;
+        sliderX.maxValue = sliderY.maxValue = sliderZ.maxValue = maxPos;
+    
+        sliderX.value = initialPos.x; 
+        sliderY.value = initialPos.y; 
+        sliderZ.value = initialPos.z;
 
-    public void Rotation(){
-        rotate = !rotate;
-        //sliderX.value = sliderY.value = sliderZ.value = 0;
+        posOrRot.text = "Position";
+    }
 
-        if(rotate){
-            //Save object position
-            initialPos = selectedObject.transform.localPosition;
+    //Init slider value for allowing to change rotation of gameObject
+    void initForChangeRot() {
+        sliderX.minValue = sliderY.minValue = sliderZ.minValue = minRotate;
+        sliderX.maxValue = sliderY.maxValue = sliderZ.maxValue = maxRotate;
 
-            sliderX.minValue = sliderY.minValue = sliderZ.minValue = minRotate;
-            sliderX.maxValue = sliderY.maxValue = sliderZ.maxValue = maxRotate;
+        sliderX.value = initialRot.x;
+        sliderY.value = initialRot.y;
+        sliderZ.value = initialRot.z;
 
-            sliderX.value = sliderY.value = sliderZ.value = 0;
-        }
-        else {
-            //Restore object position
-            selectedObject.transform.localPosition = initialPos;
-
-            sliderX.minValue = sliderY.minValue = sliderZ.minValue = minPos;
-            sliderX.maxValue = sliderY.maxValue = sliderZ.maxValue = maxPos;
-            
-            sliderX.value = initialPos.x; 
-            sliderY.value = initialPos.y; 
-            sliderZ.value = initialPos.z;
-        }
-
-        Debug.Log("Rotate is "+rotate);
+        posOrRot.text = "Rotation";
     }
 
     public void SelectObject(GameObject newObject){
         selectedObject = newObject;
+        initialPos = newObject.transform.localPosition;
+        initialRot = newObject.transform.localRotation.eulerAngles;
+
+        rotate = false;
+        objectIsSelect = true;
+
+        initForChangePos();
     }
 }
