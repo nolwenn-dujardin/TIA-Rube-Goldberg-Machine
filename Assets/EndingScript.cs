7d63e9bcd7e9fdd121de41b8ba976ff9225c4865
Dujardin Nolwenn <nolwenn.dujardin@etudiant.univ-rennes1.fr>
2024-01-15 19:08:22 +0100

    Suite parcours

---
+++ Assets/EndingScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EndingScript : MonoBehaviour
+{
+    public UnityEvent activateEnding;
+
+    private void OnTriggerEnter(Collider other) {
+        if(GameManager.Manager.isStart && other.CompareTag("ConveyorActivator")){
+            activateEnding.Invoke();
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+    }
+}
