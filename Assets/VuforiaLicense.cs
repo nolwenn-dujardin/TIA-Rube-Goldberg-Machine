3c58330e76e1d0e8deb6394c9fd7b69f018b1f9b
madelalande <maxime.delalande@etudiant.univ-rennes1.fr>
2024-01-17 17:10:37 +0100

    Add help presets.

---
+++ Assets/VuforiaLicense.cs
@@ -0,0 +1,14 @@
+
+            //This class is auto-generated, please do not overwrite
+            using UnityEngine.Scripting;
+
+            public class VuforiaLicense
+            {
+const string license = "QVl5aHZxei8vLy8vQUFBQm1lamplalQ2SUVhQWpWSXhKUG1ubWkxd3lGOVFwY21WdTV1Y3ptT0wvdnhqRVgvZGlmeWx4MGNLUDNJTFhyVmV5VlcvRWtpcTdnNXRnWnc1S2NvR2xFazVGc0xhdWdhcWZGclptd0toVDYwL0QzM1VOZDdkblVJR1FGSTJLOXh2Mjk2VDlsZ0FBTnRRWDNWd0NIdC9sZFk2WDVtd0pBVGlvb3V5UVVtVUZNcDYrT0FDTHVVZzJtYkxNTHJYKzRHd1V3bFQrMThhVVpWODdOK25jVWdhRkN3NVpBMFQ1UURqRkhJaFNIZTV6VkNnUTl3R2d6QjlJelpOZnpXV1ZwQnByZWNYRGJmZmloYm1xWGpUckdDcS9FRnJQakE3QTBKNzk0UjdsL093d2k3cC9TbHV2LzBoMXAvSTBpVHVoUzNnZFhGdnE3YUlJdW5mYWNPUHdhNTVZUjFuMHdBL3JhL2FCRENmbTE1djlWamw=";
+
+                [Preserve]
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
